9472b2f13841c52146cdb4b80cbd052b5faa0a8a
SARAL <saxenasaral@gmail.com>
2020-04-09 21:34:37 +0530

    Initial commit which will be used in google cloud

---
+++ spring-boot-lazy-init-example/src/main/java/com/saral/lazy/springbootlazyinitexample/LazyController.java
@@ -0,0 +1,21 @@
+package com.saral.lazy.springbootlazyinitexample;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lazy")
+public class LazyController {
+
+    //    @Lazy
+    @Autowired
+    LazyService lazyService;
+
+
+    @GetMapping
+    public String hello() {
+        return lazyService.hello();
+    }
+}
